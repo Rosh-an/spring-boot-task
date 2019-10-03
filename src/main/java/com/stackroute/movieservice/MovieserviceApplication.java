7f25a94794ea434704be9aa6d5abc1fe36d59e6f
Roshan <mroshan54@gmail.com>
2019-10-03 15:04:23 +0530

    api documentation using swagger 2

--- src/main/java/com/stackroute/movieservice/MovieserviceApplication.java
+++ src/main/java/com/stackroute/movieservice/MovieserviceApplication.java
@@ -2,8 +2,15 @@ package com.stackroute.movieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 //@SpringBootApplication
 @Configuration
@@ -15,4 +22,16 @@ public class MovieserviceApplication {
 		SpringApplication.run(MovieserviceApplication.class, args);
 	}
 
+}
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()                 .apis(RequestHandlerSelectors.basePackage("com.stackroute.movieservice"))
+                .paths(regex("/movie.*"))
+                .build();
+    }
 }
