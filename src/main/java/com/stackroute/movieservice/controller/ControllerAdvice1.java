d5e9e8f18eb2bfa51a4ad6ee00912de9ee6cf1cd
Roshan <mroshan54@gmail.com>
2019-10-03 17:42:57 +0530

    Global Error

---
+++ src/main/java/com/stackroute/movieservice/controller/ControllerAdvice1.java
@@ -0,0 +1,16 @@
+package com.stackroute.movieservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdvice1 {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception e) {
+        return new ResponseEntity<>("Global Error : " + e.getMessage(), HttpStatus.CONFLICT);
+    }
+}
+
